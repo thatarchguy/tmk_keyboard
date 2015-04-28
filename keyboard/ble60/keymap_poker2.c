0ccc69a2af4efd2e85a03d17d040ca9819567ba7
jichuntao <jichuntao@hotmail.com>
2015-04-29 00:07:17 +0800

    fix num

--- keyboard/ble60/keymap_poker2.c
+++ keyboard/ble60/keymap_poker2.c
@@ -19,12 +19,12 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * |Ctrl|Gui |Alt |      Space             |Alt |Fn0 |Gui |Ctrl|
      * `-----------------------------------------------------------'
      */
-    KEYMAP(
+    KEYMAP_ANSI(
         ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
-        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,NO,  ENT,  \
-        LSFT,NO,  Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,FN0, RSFT, \
-        LCTL,LGUI,LALT,          SPC,                     RALT,RGUI,FN0,RCTL),
+        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,      ENT,  \
+        LSFT,     Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,      RSFT, \
+        LCTL,LGUI,LALT,          SPC,                     RALT,RGUI,FN0 ,RCTL),
     /* Keymap 1: Fn Layer
      * ,-----------------------------------------------------------.
      * |  `| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|Delete |
@@ -38,22 +38,19 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
      * |    |    |    |                        |    |    |    |    |
      * `-----------------------------------------------------------'
      */
-    KEYMAP(
+    KEYMAP_ANSI(
         GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, DEL,  \
-        FN7, FN4, UP,  FN6,TRNS,TRNS,CALC,TRNS,INS, TRNS,PSCR,SLCK,PAUS,FN5, \
-        TRNS,LEFT,DOWN,RGHT,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,HOME,PGUP,NO,  TRNS, \
-        TRNS,NO,  TRNS,APP, FN1, FN2, FN3, VOLD,VOLU,MUTE,END, PGDN,TRNS,TRNS, \
+        FN7,FN4,UP,  FN6,TRNS,TRNS,CALC,TRNS,INS, TRNS,PSCR,SLCK,PAUS,FN5, \
+        TRNS,LEFT,DOWN,RGHT,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,HOME,PGUP,      TRNS, \
+        TRNS,     TRNS,APP, FN1, FN2, FN3, VOLD,VOLU,MUTE,END, PGDN,    TRNS, \
         TRNS,TRNS,TRNS,          TRNS,                    TRNS,TRNS,TRNS,TRNS),
 };
 
 /*
  * Fn action definition
  */
-#ifdef KEYMAP_SECTION_ENABLE
-const uint16_t fn_actions[FN_ACTIONS_COUNT] __attribute__ ((section (".keymap.fn_actions"))) = {
-#else
+
 const uint16_t fn_actions[] PROGMEM = {
-#endif
     /* Poker2 Layout */
     [0] = ACTION_LAYER_MOMENTARY(1),
     [1] = ACTION_BACKLIGHT_DECREASE(),
@@ -65,12 +62,42 @@ const uint16_t fn_actions[] PROGMEM = {
     [7] = ACTION_DISCONT_BLE()
 };
 
-#ifdef KEYMAP_IN_EEPROM_ENABLE
-uint16_t keys_count(void) {
-    return sizeof(keymaps) / sizeof(keymaps[0]) * MATRIX_ROWS * MATRIX_COLS;
-}
 
-uint16_t fn_actions_count(void) {
-    return sizeof(fn_actions) / sizeof(fn_actions[0]);
+/*
+ * user defined action function
+ */
+enum function_id {
+    CTRL_SPACE_I,       // Ctrl + Up(SpaceFN) -> PgUp
+};
+
+void action_function(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+#   define MODS_CTRL_MASK   (MOD_BIT(KC_LCTRL)|MOD_BIT(KC_RCTRL))
+    static uint8_t ctrl_space_i_prev_ctrl;
+
+    switch (id) {
+        // Ctrl + Up(SpaceFN) -> PgUp
+        case CTRL_SPACE_I:
+            ctrl_space_i_prev_ctrl = get_mods()&MODS_CTRL_MASK;
+            if (record->event.pressed) {
+                if (ctrl_space_i_prev_ctrl) {
+                    del_mods(ctrl_space_i_prev_ctrl);   // remove Ctrl
+                    add_key(KC_PGUP);
+                    send_keyboard_report(); // send PgUp without Ctrl
+                    add_mods(ctrl_space_i_prev_ctrl);   // return Ctrl but not sent
+                } else {
+                    add_key(KC_UP);
+                    send_keyboard_report();
+                }
+            } else {
+                if (ctrl_space_i_prev_ctrl) {
+                    del_key(KC_PGUP);
+                    send_keyboard_report();
+                } else {
+                    del_key(KC_UP);
+                    send_keyboard_report();
+                }
+            }
+            break;
+    }
 }
-#endif
